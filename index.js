62da5923492a3629e57105f156822d02f4214558
HW Sung <hwsung@HWui-iMac.local>
2021-12-14 00:33:00 +0900

    1.1

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require('./models/User');
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// application/json
+app.use(express.json());
+
+
+
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://shwo9:tlstm123@cluster0.w3gtj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) =>{
+    // 회원가입을 위한 route
+    // 회원가입 정보를 client에서 가져와 DB에 넣는 작업.
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
 })
 
 app.listen(port, () => {
